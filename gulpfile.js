0deed97fe57e6cbce701ea1028cf754b7c9b38c6
esk.mogorodnik <esk.mogorodnik@gmail.com>
2017-02-24 13:24:52 +0200

    sass bower html watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 "use strict";
-var browserSync = require('browser-sync'); // Подключаем Browser Sync
-var reload = browserSync.reload; //
+var browserSync = require('browser-sync').create(); // Подключаем Browser Sync
+var reload = browserSync.reload; // var for short "browserSync.reload"
 var gulp = require('gulp'); // Подключаем Gulp
-var sass = require('gulp-sass'); //Подключаем Sass пакет,
+var sass = require('gulp-sass'); //Подключаем Sass пакет
 var wiredep = require('wiredep').stream;
 
 
@@ -22,30 +22,56 @@ gulp.task('hi', function() {
 });
 
 
-// WORKING START BROWSER-SYNC SERVER
+// START BROWSER-SYNC SERVER
 //**********************************************************************
 gulp.task('serve', function() {// Создаем таск serve
-    browserSync(config); // Выполняем browser Sync
+    browserSync.init(config); // Выполняем browser Sync
 });
 
-gulp.task('sass', function() {
-    return gulp.src('src/sass/**/*.scss')
+/* DIMA */
+// gulp.task('sass', function() {
+//     return gulp.src('src/sass/**/*.scss')
+//         .pipe(sass())
+//         .pipe(gulp.dest('src/css'))
+//         .pipe(browserSync.reload({ stream: true }))
+// });
+
+/* MAX */
+gulp.task('sass', function () {
+    return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.reload({ stream: true }))
+        .pipe(browserSync.stream());
+});
+gulp.task('sass:watch', function () {
+    gulp.watch('src/scss/**/*.scss', ['sass']);
 });
 
 
+
+/* BOWER INJECT CSS&JS ROUTE FROM PLUGINS */
 gulp.task('bower', function () {
     gulp.src('src/index.html')
         .pipe(wiredep())
         .pipe(gulp.dest('src'))
 });
+gulp.task('bower:watch', function() {
+    gulp.watch('bower.json', ['bower']);
+});
 
-gulp.task('watch', ['serve', 'sass'], function() {
-    gulp.watch('src/sass/**/*.scss', ['sass']);
-    gulp.watch('src/**/*.html', reload);
-    gulp.watch('src/js/**/*.js', reload);
+/* DIMA */
+// gulp.task('watch', ['serve', 'sass'], function() {
+//     gulp.watch('src/sass/**/*.scss', ['sass']);
+//     gulp.watch('src/**/*.html', reload);
+//     gulp.watch('src/js/**/*.js', reload);
+//     gulp.watch('bower.json', ['bower']);
+// });
+//
+// gulp.task('default', ['watch']);
+/* MAX */
+gulp.task('watch', ['serve', 'bower', 'sass'], function () {
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
     gulp.watch('bower.json', ['bower']);
 });
 
